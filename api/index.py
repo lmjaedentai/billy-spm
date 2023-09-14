d01ce106dd67ba1947d38b62b68ecbf705e795c5
Jaeden Tai <taijaeden@gmail.com>
2023-09-14 18:02:25 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,38 +1,70 @@
-print('==========start==========')
-import os
-import sys
-import asyncio
-import datetime
-from pyrogram import Client,filters, errors
-from pyrogram.types import Message
+from flask import Flask
 
 
-#QQ before we start
-app = Client("BillyKaiChengBot",api_id="17817209",api_hash=os.environ['API'],bot_token=os.environ['TOKEN'])
-try:
-    with app:
-        app.send_message(-1001518766606, "#login\ndevice: [server](https://replit.com/@lmjaedentai/billy-telegram#main.py)", disable_web_page_preview=True,disable_notification=True,reply_markup=ReplyKeyboardRemove(),reply_to_message_id=179723)
-        print('==========login==========')
-except errors.exceptions.not_acceptable_406.AuthKeyDuplicated:
-    os.remove("verypigbot.session")
-    sys.exit('[shutdown] session file error')
+app = Flask(__name__)
 
-#QQ user code
-async def sleep_until(hour: int, minute: int, second: int):
-    t = datetime.datetime.today()
-    future = datetime.datetime(t.year, t.month, t.day, hour, minute, second)
-    if t.timestamp() > future.timestamp():
-        future += datetime.timedelta(days=1)
-    await asyncio.sleep((future - t).total_seconds())
 
-def sendcountdown():
-    difference = datetime.datetime(2024, 1, 30) - datetime.datetime.now()
-    return difference.days+1
+@app.route('/')
+def home():
+    return 'Home Page Route'
 
-async def main():
-    async with app:
-        while True:
-            await app.send_message(-1001197820173, f"**SPM**\n{sendcountdown()} days left")
-            await sleep_until(12,21,0)
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
+
+@app.route('/api')
+def api():
+    with open('data.json', mode='r') as my_file:
+        text = my_file.read()
+        return text
+
+# print('==========start==========')
+# import os
+# import sys
+# import asyncio
+# import datetime
+# from pyrogram import Client,filters, errors
+# from pyrogram.types import Message
+
+
+# #QQ before we start
+# app = Client("BillyKaiChengBot",api_id="17817209",api_hash=os.environ['API'],bot_token=os.environ['TOKEN'])
+# try:
+#     with app:
+#         app.send_message(-1001518766606, "#login\ndevice: [server](https://replit.com/@lmjaedentai/billy-telegram#main.py)", disable_web_page_preview=True,disable_notification=True,reply_markup=ReplyKeyboardRemove(),reply_to_message_id=179723)
+#         print('==========login==========')
+# except errors.exceptions.not_acceptable_406.AuthKeyDuplicated:
+#     os.remove("verypigbot.session")
+#     sys.exit('[shutdown] session file error')
+
+# #QQ user code
+# async def sleep_until(hour: int, minute: int, second: int):
+#     t = datetime.datetime.today()
+#     future = datetime.datetime(t.year, t.month, t.day, hour, minute, second)
+#     if t.timestamp() > future.timestamp():
+#         future += datetime.timedelta(days=1)
+#     await asyncio.sleep((future - t).total_seconds())
+
+# def sendcountdown():
+#     difference = datetime.datetime(2024, 1, 30) - datetime.datetime.now()
+#     return difference.days+1
+
+# async def main():
+#     async with app:
+#         while True:
+#             await app.send_message(-1001197820173, f"**SPM**\n{sendcountdown()} days left")
+#             await sleep_until(12,21,0)
             
-app.run(main())
+# app.run(main())
